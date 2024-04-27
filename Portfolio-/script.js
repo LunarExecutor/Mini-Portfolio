0d30a5a2dfe7ba9ae3389a2cf8d8677acaa0edb7
Luna <87090986+LunarExecutor@users.noreply.github.com>
2024-04-27 09:40:03 +0300

    Delete Portfolio- directory

--- Portfolio-/script.js
+++
@@ -1,8 +0,0 @@
-// Select the dark mode switch
-const darkModeToggle = document.querySelector('.switch input');
-
-// Add event listener to toggle dark mode
-darkModeToggle.addEventListener('change', () => {
-    // Toggle dark mode class on the body
-    document.body.classList.toggle('slider');
-});
