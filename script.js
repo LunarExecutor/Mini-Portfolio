fe994f0235f88d7b8c577f3620f9c694362534bc
LunarExecutor <laghauna@gmail.com>
2024-04-26 23:18:30 -0700

    all changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById('menu-icon').addEventListener('click', function() {
     var nav = document.querySelector('.nav-bar ul');
     nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
@@ -15,3 +16,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+=======
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var nav = document.querySelector('.nav-bar ul');
+    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+});
+
+
+
+// Smooth scrolling when clicking on navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+>>>>>>> b7021a2 (all changes)
