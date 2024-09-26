74671e802c6b05f7af4a075099e8b49d50b8a277
LunarExecutor <laghauna@gmail.com>
2024-09-26 13:45:36 -0700

    some changes in JS file

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-document.getElementById('menu-icon').addEventListener('click', function() {
-    var nav = document.querySelector('.nav-bar ul');
-    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
-});
-
-
-
-// Smooth scrolling when clicking on navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-=======
 document.getElementById('menu-icon').addEventListener('click', function() {
     var nav = document.querySelector('.nav-bar ul');
     nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
